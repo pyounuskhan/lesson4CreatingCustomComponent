9d468e4fcb4bfbdf43048458739e6ad6e6ca25f6
pyounuskhan <pyounuskhan@gmail.com>
2018-03-06 19:12:04 +0530

    Initial commit

---
+++ app/src/main/java/ifirst/co/lesson4creatingcustomcomponent/CustomView.java
@@ -0,0 +1,27 @@
+package ifirst.co.lesson4creatingcustomcomponent;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.view.View;
+
+/**
+ * Created by Ibrahim on 06-03-2018.
+ */
+
+public class CustomView extends View {
+    final Paint mPaint = new Paint();
+
+    public CustomView(Context context) {
+        super(context);
+        mPaint.setColor(Color.BLACK);
+        mPaint.setTextSize(30);
+    }
+
+    @Override protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        setBackgroundColor(Color.CYAN);
+        canvas.drawText("Custom Text", 100, 100, mPaint);
+        invalidate(); }
+}
